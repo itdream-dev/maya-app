7eccc58822925f42835f3101332c066cf1a1c2df
skyclean906 <skyclean906@gmail.com>
2020-09-12 00:10:01 +0800

    update

---
+++ src/pages/Compound/vote.tsx
@@ -0,0 +1,27 @@
+
+import React from 'react'
+
+import AppBody from '../AppBody'
+import { SwapPoolTabs } from '../../components/NavigationTabs'
+import { Wrapper } from '../../components/swap/styleds'
+export default function Vote() {
+
+  
+
+
+
+  // show approve flow when: no error on inputs, not approved or pending, or approved in current session
+  // never show if price impact is above threshold in non expert mode
+
+
+  return (
+    <>
+      <AppBody>
+        <SwapPoolTabs active={'vote'} />
+        <Wrapper id="vote-page">
+          
+        </Wrapper>
+      </AppBody>      
+    </>
+  )
+}
